9c0a663611244cd5fb077a0fae5ef49a571397a2
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2021-09-22 14:58:18 +0800

    Added files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function headerSizeUpdate() {
+  var header = document.getElementsByClassName('header')[0];
+  var menu = document.getElementsByClassName('menu')[0];
+  if (header) {
+    var rect1 = header.getBoundingClientRect();
+  }
+  if (menu) {
+    var rect2 = menu.getBoundingClientRect();
+  }
+  if (rect1 && rect2) {
+    if (rect1.bottom - rect2.bottom < 50)
+    {
+      header.style.height = (String(rect2.bottom+50)).concat("px");
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  var canvas = document.getElementById("acanvas");
+  if (document.getElementsByClassName('theme-container')[0]) {
+    document.getElementsByClassName('theme-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+    document.getElementsByClassName('theme-info-container')[0].style.background="url("+canvas.toDataURL()+") fixed";
+  }
+  document.body.onresize=headerSizeUpdate();
+});
+
+window.addEventListener('orientationchange', headerSizeUpdate());
